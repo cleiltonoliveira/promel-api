1b8929f6e5acbdd6a340182283b25fe3c4cf0359
cleiltonoliveira <clltnoliveira@gmail.com>
2021-08-20 08:42:45 -0300

    First commit

---
+++ src/main/java/com/promel/api/PromelApiApplication.java
@@ -0,0 +1,13 @@
+package com.promel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/promel/api/PromelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
