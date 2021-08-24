9f7cad34b95e70b67ea7c456c08d9b1d90b75f84
cleiltonoliveira <clltnoliveira@gmail.com>
2021-08-24 16:51:30 -0300

    Add missing repository annotation

--- src/main/java/com/promel/api/repository/UserAuthRepository.java
+++ src/main/java/com/promel/api/repository/UserAuthRepository.java
@@ -2,9 +2,11 @@ package com.promel.api.repository;
 
 import com.promel.api.model.UserAuth;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserAuthRepository extends CrudRepository<UserAuth, Long> {
 
     boolean existsByEmail(String email);
