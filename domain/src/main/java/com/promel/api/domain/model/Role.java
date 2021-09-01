e94009cc52d08ee7b6692a6d3087619e98386b2c
cleiltonoliveira <clltnoliveira@gmail.com>
2021-08-31 21:33:03 -0300

    Add lombok to domain models

--- domain/src/main/java/com/promel/api/domain/model/Role.java
+++ domain/src/main/java/com/promel/api/domain/model/Role.java
@@ -1,46 +1,10 @@
 package com.promel.api.domain.model;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Role {
     private Long id;
     private String role;
     private String details;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role1 = (Role) o;
-        return id.equals(role1.id) && role.equals(role1.role) && Objects.equals(details, role1.details);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, role, details);
-    }
 }
