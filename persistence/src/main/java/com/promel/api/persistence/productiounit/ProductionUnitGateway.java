cc118f96f770354046cffcda2294b60f5033f0df
cleiltonoliveira <clltnoliveira@gmail.com>
2021-10-22 16:44:58 -0300

    Add endpoint to create production units

---
+++ persistence/src/main/java/com/promel/api/persistence/productiounit/ProductionUnitGateway.java
@@ -0,0 +1,35 @@
+package com.promel.api.persistence.productiounit;
+
+import com.promel.api.domain.model.ProductionUnit;
+import com.promel.api.usecase.productiounit.adapter.ProductionUnitAdapter;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class ProductionUnitGateway implements ProductionUnitAdapter {
+
+    private ProductionUnitRepository repository;
+    private ModelMapper modelMapper;
+
+    @Override
+    public Optional<ProductionUnit> findById(Long id) {
+        return repository.findById(id).map(this::toDomain);
+    }
+
+    @Override
+    public ProductionUnit save(ProductionUnit productionUnit) {
+        return toDomain(repository.save(toEntity(productionUnit)));
+    }
+
+    private ProductionUnit toDomain(ProductionUnitEntity entity) {
+        return modelMapper.map(entity, ProductionUnit.class);
+    }
+
+    private ProductionUnitEntity toEntity(ProductionUnit domain) {
+        return modelMapper.map(domain, ProductionUnitEntity.class);
+    }
+}
---
+++ web/src/main/java/com/promel/api/web/controller/productionunit/dto/ProductionUnitResponse.java
@@ -0,0 +1,12 @@
+package com.promel.api.web.controller.productionunit.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ProductionUnitResponse {
+    private Long id;
+    private Double totalProduction;
+    private LocalDateTime lastModificationDate;
+}
