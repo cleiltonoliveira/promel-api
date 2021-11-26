fde7ac0ee82e676c0a00601a063e09f59edff726
Cleilton Oliveira <51163079+cleiltonoliveira@users.noreply.github.com>
2021-11-26 12:51:12 -0300

    Add endpoint to get a user production unit (#21)

--- usecase/src/main/java/com/promel/api/usecase/productiounit/ProductionUnitFinder.java
+++ usecase/src/main/java/com/promel/api/usecase/productiounit/ProductionUnitFinder.java
@@ -3,6 +3,7 @@ package com.promel.api.usecase.productiounit;
 import com.promel.api.domain.model.ProductionUnit;
 import com.promel.api.usecase.exception.ResourceNotFoundException;
 import com.promel.api.usecase.productiounit.adapter.ProductionUnitAdapter;
+import com.promel.api.usecase.user.UserAccountFinder;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -11,13 +12,19 @@ import javax.inject.Named;
 public class ProductionUnitFinder {
 
     private ProductionUnitAdapter adapter;
+    private UserAccountFinder userAccountFinder;
 
     @Inject
-    public ProductionUnitFinder(ProductionUnitAdapter adapter) {
+    public ProductionUnitFinder(ProductionUnitAdapter adapter, UserAccountFinder userAccountFinder) {
         this.adapter = adapter;
+        this.userAccountFinder = userAccountFinder;
     }
 
     public ProductionUnit findById(Long id) {
         return adapter.findById(id).orElseThrow(() -> new ResourceNotFoundException("Production unit not found"));
+    }
+
+    public ProductionUnit findByUser(String email) {
+        return adapter.findFirstByUserId(userAccountFinder.findByEmail(email).getId()).orElseThrow(() -> new ResourceNotFoundException("Production unit not found"));
     }
 }
--- web/src/main/java/com/promel/api/web/controller/productionunit/ProductionUnitController.java
+++ web/src/main/java/com/promel/api/web/controller/productionunit/ProductionUnitController.java
@@ -2,11 +2,13 @@ package com.promel.api.web.controller.productionunit;
 
 import com.promel.api.domain.model.ProductionUnit;
 import com.promel.api.usecase.productiounit.ProductionUnitCreator;
+import com.promel.api.usecase.productiounit.ProductionUnitFinder;
 import com.promel.api.web.controller.productionunit.dto.ProductionUnitResponse;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +21,19 @@ import java.security.Principal;
 public class ProductionUnitController {
 
     private ProductionUnitCreator productionUnitCreator;
+    private ProductionUnitFinder productionUnitFinder;
     private ModelMapper modelMapper;
 
     @PostMapping("protected/production-units")
     public ResponseEntity<?> createProductionUnit(Principal principal) {
         var unit = productionUnitCreator.create(principal.getName());
         return new ResponseEntity<>(toProductionUnitResponse(unit), HttpStatus.CREATED);
+    }
+
+    @GetMapping("protected/production-units/unit")
+    public ResponseEntity<?> findUserProductionUnit(Principal principal) {
+        var unit = productionUnitFinder.findByUser(principal.getName());
+        return new ResponseEntity<>(toProductionUnitResponse(unit), HttpStatus.OK);
     }
 
     private ProductionUnitResponse toProductionUnitResponse(ProductionUnit unit) {
