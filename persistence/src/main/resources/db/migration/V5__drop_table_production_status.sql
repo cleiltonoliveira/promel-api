82714ae6f553c40bc5c3dc4dde6bd43454112ec7
cleiltonoliveira <clltnoliveira@gmail.com>
2021-12-07 16:37:36 -0300

    Fix inconsistencies in migration files

--- persistence/src/main/resources/db/migration/V5__drop_table_production_status.sql
+++ persistence/src/main/resources/db/migration/V5__drop_table_production_status.sql
@@ -1,4 +1,6 @@
 ALTER TABLE `honey_production`
+DROP FOREIGN KEY `fk_harvest_production_status1`;
+ALTER TABLE `honey_production`
 DROP COLUMN `production_status_id`,
 DROP INDEX `fk_harvest_production_status1_idx` ;
 
